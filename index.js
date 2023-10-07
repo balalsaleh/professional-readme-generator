087ef8b0d6b42a4079f4c34ea8b123d7ba8d9c43
Balal Saleh <balal_saleh@outlook.com>
2023-10-07 01:14:16 +0100

    adding more questions to test the readmegeneration

--- index.js
+++ index.js
@@ -3,31 +3,71 @@ const fs = require('fs');
 
 // questions array for readme
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Enter the title of your project:',
-    }
-    // i need to add:
-    /* 
-Sections entitled:
-Description
-Table of Contents
-Installation
-Usage
-License
-Contributing
-Tests
-Questions
-    */
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of your project:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license for your project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your questions:',
+    },
 ];
 
 // generating the readme file
 function generateReadme(replies) {
     return `
-  
 # ${replies.title}
-
+# ${replies.description}
+# ${replies.installation}
+# ${replies.usage}
+# ${replies.license}
+# ${replies.contributing}
+# ${replies.tests}
+# ${replies.github}
+# ${replies.email}
+# ${replies.questions}
 `;
 }
 
@@ -35,28 +75,6 @@ inquirer.prompt(questions).then((replies) => {
   const readmeContent = generateReadme(replies);
 
   //writingto the README.md file
-  fs.writeFileSync('README.md', readmeContent);
-  console.log('README.md created');
+  fs.writeFileSync('generatedReadME.md', readmeContent);
+  console.log('generatedReadME.md created');
 });
-
-/*
-Create a command-line application that accepts user input.
-When a user is prompted for information about the application repository then a high-quality, professional README.md is generated with:
-
-The title of my project
-Sections entitled:
-Description
-Table of Contents
-Installation
-Usage
-License
-Contributing
-Tests
-Questions
-When a user enters the project title then it is displayed as the title of the README
-When a user enters a description, installation instructions, usage information, contribution guidelines, and test instructions then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
-When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
-When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
-*/
