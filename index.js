3007d1b1c87aaeec0ee653034ebbe9696b17bc45
Balal Saleh <balal_saleh@outlook.com>
2023-10-10 22:31:18 +0100

    adding the license capability

--- index.js
+++ index.js
@@ -1,68 +1,104 @@
+// Required dependencies
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// questions array for readme
+// Questions array for gathering project information
 const questions = [
+    // Prompt for the project title
     {
         type: 'input',
         name: 'title',
         message: 'Enter the title of your project:',
     },
+    // Prompt for the project description
     {
         type: 'input',
         name: 'description',
         message: 'Enter a description for your project:',
     },
+    // Prompt for installation instructions
     {
         type: 'input',
         name: 'installation',
         message: 'Enter installation instructions:',
     },
+    // Prompt for usage information
     {
         type: 'input',
         name: 'usage',
         message: 'Enter usage information:',
     },
+    // Prompt for choosing a license
     {
         type: 'list',
         name: 'license',
         message: 'Choose a license for your project:',
         choices: ['MIT', 'Apache', 'GPL', 'None'],
     },
+    // Prompt for contribution guidelines
     {
         type: 'input',
         name: 'contributing',
         message: 'Enter contribution guidelines:',
     },
+    // Prompt for test instructions
     {
         type: 'input',
         name: 'tests',
         message: 'Enter test instructions:',
     },
+    // Prompt for the user's GitHub username
     {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub username:',
     },
+    // Prompt for the user's email address
     {
         type: 'input',
         name: 'email',
         message: 'Enter your email address:',
     },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Enter your questions:',
-    },
 ];
 
-// generating the readme file
+// Function to generate license badge and notice
+function generateLicenseInfo(license) {
+    let licenseBadge, licenseNotice;
+
+    switch (license) {
+        case 'MIT':
+            licenseBadge = '[![MIT License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)';
+            licenseNotice = 'This project is licensed under the MIT License.';
+            break;
+        case 'Apache':
+            licenseBadge = '[![Apache License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)';
+            licenseNotice = 'This project is licensed under the Apache License 2.0.';
+            break;
+        case 'GPL':
+            licenseBadge = '[![GPL License](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            licenseNotice = 'This project is licensed under the GNU General Public License (GPL) version 3.';
+            break;
+        case 'None':
+            licenseBadge = ''; 
+            licenseNotice = 'This project is not licensed.';
+            break;
+        default:
+            licenseBadge = '';
+            licenseNotice = '';
+    }
+
+    return { badge: licenseBadge, notice: licenseNotice };
+}
+
+// Function to generate the content of the README file
 function generateReadme(replies) {
+    // Generate license info
+    const { badge, notice } = generateLicenseInfo(replies.license); 
+
     return `
-
-# README
-
 # ${replies.title}
+
+${badge}
 
 ## Description
 ${replies.description}
@@ -82,7 +118,7 @@ ${replies.installation}
 ${replies.usage}
 
 ## License
-This project is using the ${replies.license} License.
+${notice}
 
 ## Contributing
 ${replies.contributing}
@@ -90,7 +126,7 @@ ${replies.contributing}
 ## Tests
 ${replies.tests}
 
-##Questions
+## Questions
 
 GitHub: [${replies.github}](https://github.com/${replies.github})
 
@@ -98,12 +134,13 @@ For additional questions, you can reach me at ${replies.email}.
 `;
 }
 
+// Prompt the user with questions, generate the README, and save it to a file
 inquirer.prompt(questions).then((replies) => {
-  const readmeContent = generateReadme(replies);
+    const readmeContent = generateReadme(replies);
 
-  //writing to the generatedREADME.md file and put in a seperate generated folder
-  fs.writeFileSync('./generated/generatedREADME.md', readmeContent);
-  console.log('Check inside the generated folder for the generatedREADME.md you have just created');
-}) .catch((error) => {
+    // Write the generated README content to a file and place it in a separate "generated" folder
+    fs.writeFileSync('./generated/generatedREADME.md', readmeContent);
+    console.log('Check inside the generated folder for the generatedREADME.md you have just created');
+}).catch((error) => {
     console.error('An ERROR has taken place', error);
-  })
+});
